72489b56fcd081deba65b2e917515265647d07f0
karthik <karthik.krishnaswamy17@gmail.com>
2022-01-19 18:04:54 +0530

    First commit of simple ansible playbook

---
+++ docker_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xv
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker ubuntu
